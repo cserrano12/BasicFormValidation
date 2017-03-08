e7462563236e9e64667a925954a5a73ba3e5faf6
Unknown <cserrano12@mail.csuchico.edu>
2017-03-07 22:45:57 -0800

    Validation of username and password

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,29 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
+if ( userEntered.length < 6 ){
   //Show message that there is an error with the username...
   document.getElementById("usernameError").innerHTML="Bad username.";
   document.getElementById("usernameError").classList.remove("hidden-message");
   document.getElementById("usernameError").classList.add("shown-message");
   //Turn the username items red
   document.getElementById("usernameGroup").classList.add("has-error");
+  }
+else {
+  document.getElementById("usernameGroup").classList.add("has-success");
+  document.getElementById("usernameError").innerHTML="Username OK";
+  }
 
+if ( passEntered != "password" ){
+  document.getElementById("passwordGroup").classList.add("has-success");
+  document.getElementById("passwordError").innerHTML="Password OK";
+  }
+else {
   //Show message that there is an error with the password...
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
 }
